81a94bbdc99c3a1bf8eb0656b39edc1395062b80
luucasxd <lucasmelodos322@gmail.com>
2019-10-26 22:15:30 -0300

    melhoria da interacao

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "fila_fixa.h"
+#include "filaListaEncadeada.h"
 #define MAXTAM 5
 int menu(void){
     int opcao;
@@ -12,9 +12,10 @@ int menu(void){
 }
 int main(){
     
-int opcao,elemento,cont = 0,j;
+int opcao,elemento,vetor[MAXTAM];
 int n;
 criar_fila();
+int cont = -1,j;
 while ((opcao = menu()) != 0){
 switch (opcao)
 {
@@ -22,29 +23,40 @@ case 1:
     printf("Quantos elementos deseja adicionar?\n");
     scanf("%d",&n);
     for(j=0;j<n;j++){
-    scanf("%d",&elemento);
+    scanf("%d",&vetor[j]);
+    if(&vetor[j]>=0 && vetor[j]<=900)
+        inserir_elemento(vetor[j]);
+    else
+        printf(" Elemento %d não foi inserido -> Digite um numero positivo maior que 1000\n",vetor[j]);
     if(fila_cheia()){
         cont++;
     }  
-    inserir_elemento(elemento);
     }
     if(fila_cheia() && j==n){
         printf(" %d elemento(s) foram inserido(s) na fila, mas %d não coube(ram)\n",n,cont);
     }  
     break;
 case 2:
-    remover_elemento(&elemento);
+    printf("Quantos elementos deseja remover?\n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        if(!fila_vazia()){
+            remover_elemento(vetor);
+            printf("Elemento removido: %d\n",vetor[i]);
+        }  
+        else{   
+             printf("não é possivel remover mais elementos, pois a fila está vazia\n");
+        }
+    }
     break;
 case 3:
-    exibir_fila();
+    exibir();
     break;
 default:
     break;
 }
 putchar('\n');
 }
-
-
 
 
     return 0;
