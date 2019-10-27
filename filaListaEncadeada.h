60bde23a57ea214d6aee4b2915db7c51335646db
luucasxd <lucasmelodos322@gmail.com>
2019-10-26 21:44:32 -0300

    criacao da funcao destruir_fila()

---
+++ filaListaEncadeada.h
@@ -0,0 +1,12 @@
+#ifndef FILA_FIXA_H
+#define FILA_FIXA_H
+
+void criar_fila();
+int inserir_elemento(int elemento);
+int remover_elemento(int *elemento);
+int fila_cheia();
+int fila_vazia();
+void exibir();
+int destruir_fila();
+
+#endif
